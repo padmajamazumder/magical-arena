e512fc04e20c1507954ee0b57a34fec0f1cfb3f1
Padmaja mazumder <padmajamazumder@gmail.com>
2024-05-29 18:56:49 +0530

    Include name property

--- src/Player.js
+++ src/Player.js
@@ -1,39 +1,27 @@
-/**
- * Represents a player in the magical arena.
- */
-class Player {
-    #health;
-    #strength;
-    #attack;
 
-    constructor(health, strength, attack) {
-        this.#health = health;
-        this.#strength = strength;
-        this.#attack = attack;
+
+class Player {
+    constructor(name, health, strength, attack) {
+        this.name = name;
+        this.health = health;
+        this.strength = strength;
+        this.attack = attack;
     }
 
     getHealth() {
-        return this.#health;
+        return this.health;
     }
 
-    getStrength() {
-        return this.#strength;
-    }
-
-    getAttack() {
-        return this.#attack;
-    }
-
-    reduceHealth(damage) {
-        this.#health -= damage;
-        // Ensure health doesn't drop below zero
-        if (this.#health < 0) {
-            this.#health = 0;
-        }
+    reduceHealth(amount) {
+        this.health = Math.max(0, this.health - amount); // Prevent negative health
     }
 
     isAlive() {
-        return this.#health > 0;
+        return this.health > 0;
+    }
+
+    getName() {
+        return this.name;
     }
 }
 
