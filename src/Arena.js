9ad305c07c2fef4021f79370c307f156b57cc823
Padmaja mazumder <padmajamazumder@gmail.com>
2024-05-30 08:39:11 +0530

    BUG FIX: automated dice roll

--- src/Arena.js
+++ src/Arena.js
@@ -1,3 +1,4 @@
+// src/Arena.js
 
 const Die = require('./Die');
 
@@ -25,29 +26,22 @@ class Arena {
         console.log(`${defender.getName()} takes ${damage} damage, health is now ${defender.getHealth()}\n`);
     }
 
-    async manualRollDice(player) {
-        const roll = await Die.simulateDieRoll(player.getName(), this.rl); // Pass rl instance here
-        return roll;
+    async manualRollDice(attacker, defender) {
+        const attackRoll = await Die.simulateDieRoll(attacker.getName(), this.rl);
+        const defenseRoll = await Die.simulateDieRoll(defender.getName(), this.rl);
+
+        const attackDamage = attacker.attack * attackRoll;
+        const defenseStrength = defender.strength * defenseRoll;
+
+        const damage = Math.max(0, attackDamage - defenseStrength);
+        defender.reduceHealth(damage);
+
+        console.log(`${attacker.getName()} attacks with roll ${attackRoll} (Damage: ${attackDamage})`);
+        console.log(`${defender.getName()} defends with roll ${defenseRoll} (Defense: ${defenseStrength})`);
+        console.log(`${defender.getName()} takes ${damage} damage, health is now ${defender.getHealth()}\n`);
     }
 
-    async startGameWithManualDiceRoll() {
-        console.log("The battle begins!");
-
-        while (this.player1.isAlive() && this.player2.isAlive()) {
-            await this.manualRollDice(this.player1);
-            if (this.player1.isAlive()) {
-                await this.manualRollDice(this.player2);
-            }
-        }
-
-        if (this.player1.isAlive()) {
-            console.log(`${this.player1.getName()} wins the battle!`);
-        } else {
-            console.log(`${this.player2.getName()} wins the battle!`);
-        }
-    }
-
-    startGame() {
+    async startGame() {
         console.log("The battle begins!");
 
         while (this.player1.isAlive() && this.player2.isAlive()) {
@@ -70,7 +64,30 @@ class Arena {
             console.log(`${this.player2.getName()} wins the battle!`);
         }
     }
+
+    async startGameWithManualDiceRoll() {
+        console.log("The battle begins!");
+
+        while (this.player1.isAlive() && this.player2.isAlive()) {
+            if (this.player1.getHealth() <= this.player2.getHealth()) {
+                await this.manualRollDice(this.player1, this.player2);
+                if (this.player2.isAlive()) {
+                    await this.manualRollDice(this.player2, this.player1);
+                }
+            } else {
+                await this.manualRollDice(this.player2, this.player1);
+                if (this.player1.isAlive()) {
+                    await this.manualRollDice(this.player1, this.player2);
+                }
+            }
+        }
+
+        if (this.player1.isAlive()) {
+            console.log(`${this.player1.getName()} wins the battle!`);
+        } else {
+            console.log(`${this.player2.getName()} wins the battle!`);
+        }
+    }
 }
 
 module.exports = Arena;
-
